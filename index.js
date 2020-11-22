0254241bc028a8f4573cc1de437a7b1cc8840738
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2020-11-22 11:57:34 -0600

    upgrade build

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fetch = require("isomorphic-fetch");
+const { reporters } = require("mocha");
 const report = require("./cypress-tests-report.json");
 
 try {
@@ -9,7 +11,17 @@ try {
     " on repo ",
     github.context.repo.repo
   );
-  core.setOutput("score", report.stats.passPercent);
+  fetch({
+    url: "https://cyolrzfuz3.execute-api.us-east-1.amazonaws.com/dev/grade",
+    method: "POST",
+    body: {
+      student: github.context.repo.owner,
+      project: github.context.repo.repo,
+      score: report.stats.passPercent,
+    },
+  }).then(() => {
+    core.setOutput("score", report.stats.passPercent);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
